df29d6f70c4062312cc176ed2d7364fca4a33289
AndreiWasFound <54415861+AndreiWasFound@users.noreply.github.com>
2020-05-25 18:17:44 +0300

    Code version 1.0
    This version only has NPC's that do, well, nothing, just look where your head was when you created them.

---
+++ CustomNPC/src/me/AndreiWasFound/CustomNPC/Join.java
@@ -0,0 +1,18 @@
+package me.AndreiWasFound.CustomNPC;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Join implements Listener {
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		if (NPC.getNPCs() == null)
+			return;
+		if (NPC.getNPCs().isEmpty())
+			return;
+		NPC.addJoinPacket(event.getPlayer());
+	}
+	
+}
---
+++ CustomNPC/src/me/AndreiWasFound/CustomNPC/Main.java
@@ -0,0 +1,34 @@
+package me.AndreiWasFound.CustomNPC;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(new Join(), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("createnpc")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Console can't use this command");
+				return true;
+			}
+			Player player = (Player) sender;
+			NPC.createNPC(player);
+			player.sendMessage(ChatColor.RED + "NPC has been created");
+		}
+		return false;
+	}
+}
